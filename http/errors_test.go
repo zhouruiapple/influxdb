7616f8b8814a881622fabc63d99ae94046db42b5
George MacRorie <me@georgemac.com>
2019-08-13 17:18:06 +0000

    Add failing test for text/plain content-type handling http/errors

--- http/errors_test.go
+++ http/errors_test.go
@@ -49,3 +49,68 @@ func TestEncodeErrorWithError(t *testing.T) {
 		t.Errorf("errors encode err: got %s", w.Body.String())
 	}
 }
+
+func TestCheckErrorContentType(t *testing.T) {
+	for _, testCase := range []struct {
+		name        string
+		body        []byte
+		contentType string
+		code        int
+		// expecations
+		errCode    string
+		errMessage string
+	}{
+		{
+			name:        "text/plain status code 500",
+			body:        []byte("something went wrong"),
+			contentType: "text/plain",
+			code:        500,
+			errCode:     influxdb.EInternal,
+			errMessage:  "something went wrong",
+		},
+		{
+			name:        "blank content type status code 504",
+			body:        []byte("upstream request timeout"),
+			contentType: "",
+			code:        504,
+			errCode:     influxdb.EInternal,
+			errMessage:  "upstream request timeout",
+		},
+		{
+			name:        "application/json; charset=utf-8 status code 404",
+			body:        []byte(`{"code":"not found", "error": "could not find resource"}`),
+			contentType: "application/json; charset=utf-8",
+			code:        404,
+			errCode:     influxdb.ENotFound,
+			errMessage:  "could not find resource",
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			w.Header().Set("Content-Type", testCase.contentType)
+			w.WriteHeader(testCase.code)
+			w.Write(testCase.body)
+
+			err := http.CheckError(w.Result())
+			if err == nil {
+				t.Error("err must not be nil")
+			}
+
+			ierr, ok := err.(*influxdb.Error)
+			if !ok {
+				t.Errorf("should be *influxdb.Error, got: %q", err)
+				return
+			}
+
+			if testCase.errCode != ierr.Code {
+				t.Errorf("expected %q, got %q", testCase.errCode, ierr.Code)
+				return
+			}
+
+			if testCase.errMessage != ierr.Err.Error() {
+				t.Errorf("expected %q, got %q", testCase.errMessage, ierr.Err)
+			}
+		})
+	}
+}
