9f278d73195f17714ffbc7e2a8acd9eddb6b70f4
Lyon Hill <lyondhill@gmail.com>
2020-05-22 12:05:58 -0600

    hackdaything DO NOT MERGE

--- query/influxql/math.go
+++ query/influxql/math.go
@@ -5,7 +5,7 @@ import "github.com/influxdata/influxql"
 // isMathFunction returns true if the call is a math function.
 func isMathFunction(expr *influxql.Call) bool {
 	switch expr.Name {
-	case "abs", "sin", "cos", "tan", "asin", "acos", "atan", "atan2", "exp", "log", "ln", "log2", "log10", "sqrt", "pow", "floor", "ceil", "round":
+	case "abs", "sin", "cos", "tan", "asin", "acos", "atan", "atan2", "exp", "log", "ln", "log2", "log10", "sqrt", "pow", "floor", "ceil", "round", "randInt":
 		return true
 	}
 	return false
