ac04db09cc0e844c987d9a7003dde22c581e900e
Dean Sheehan <dean.sheehan@me.com>
2020-05-23 15:16:01 +0100

    property now split into general and advanced tab

--- ui/src/dashboards/utils/RagnarokActionParametersForm.tsx
+++ ui/src/dashboards/utils/RagnarokActionParametersForm.tsx
@@ -2,6 +2,9 @@
 import React, {PureComponent, MouseEvent} from 'react'
 import {connect} from 'react-redux'
 
+import {Tabs, Orientation, ComponentSize} from '@influxdata/clockface'
+
+
 import {
     ComponentColor,
     Input,
@@ -26,48 +29,68 @@ export class RagnarokActionParametersForm extends PureComponent<Props> {
         outputTags: this.props.action.output ? this.props.action.output.defaultTags : '',
         repeat: '0s',
         parameterValues: this.getInitialParameterValues(),
+        general: true,
     }
 
     public render() {
         return (
-    
         <Overlay visible={this.props.isVisible}>
-        <Overlay.Container maxWidth={600}>
-          <Overlay.Header title={this.props.action.name + " with " + this.props.service.name}/>
-          <Overlay.Body key="foo">
-          {this.props.action.parameters.map(p => (
-           [<Form.Label key={p.name+"-l"} required={true} label={p.name}/>,
-            <Input key={p.name+"-f"} value={this.state.parameterValues[p.name]} placeholder={p.name} onChange={(e)=>{this.updateParam(p.name,e)}} />,
-            <Form.HelpText key={p.name+"-h"} text={p.description}></Form.HelpText>]
-          ))}
-        
-          <Form.Divider lineColor={ComponentColor.Primary}/>
-          <Form.Label required={true} label="Destination Bucket"/>
-          <Input value={this.state.destinationBucket} placeholder="Destination Bucket" onChange={this.updateDestinationBucket} />
-          <Form.HelpText text="The bucket to write resuls to"/>
+            <Overlay.Container maxWidth={600}>
+            <Overlay.Header title={this.props.action.name + " with " + this.props.service.name}/>
+            <Overlay.Body key="foo">
+                <Tabs.Container orientation={Orientation.Horizontal}>
+                    <Tabs orientation={Orientation.Horizontal}size={ComponentSize.ExtraSmall} dropdownBreakpoint={872}>
+                        <Tabs.Tab
+                        key="General"
+                        text="General"
+                        id="General"
+                        active={this.state.general}
+                        onClick={()=>{this.setState({general:true})}}
+                        />    
 
-          <Form.Divider lineColor={ComponentColor.Primary}/>
-          <Form.Label required={true} label="Destination Measurement"/>
-          <Input value={this.state.destinationMeasurement} placeholder="Destination Measurement" onChange={this.updateDestinationMeasurement} />
-          <Form.HelpText text="The measurement to write resuls to"/>
-
-          <Form.Divider lineColor={ComponentColor.Primary}/>
-          <Form.Label required={false} label="Output Tags"/>
-          <Input value={this.state.outputTags} placeholder="Output Tags" onChange={this.updateOutputTags}/>
-          <Form.HelpText text="Any additional tags to attach to the results"/>):
-          
-          <Form.Divider lineColor={ComponentColor.Primary}/>
-          <Form.Label required={false} label="Repeat Every"/>
-          <Input value={this.state.repeat} placeholder="How often to repeat" onChange={this.updateRepeat}/>
-          <Form.HelpText text="If required, how often should this action to be repeated?"/>
-
-          </Overlay.Body>
-          <Overlay.Footer>
-            <Button text="Apply" onClick={()=>this.props.onApply(this.state)} />
-            <Button text="Cancel" onClick={this.props.onCancel} />
-          </Overlay.Footer>
-        </Overlay.Container>
-      </Overlay>
+                        <Tabs.Tab
+                        key="Advanced"
+                        text="Advanced"
+                        id="Advanced"
+                        active={!this.state.general}
+                        onClick={()=>{this.setState({general:false})}}
+                        />    
+                    </Tabs> 
+                    <Tabs.TabContents>
+                        {this.state.general && this.props.action.parameters.map(p => (
+                            [<Form.Label key={p.name+"-l"} required={true} label={p.name}/>,
+                                <Input key={p.name+"-f"} value={this.state.parameterValues[p.name]} placeholder={p.name} onChange={(e)=>{this.updateParam(p.name,e)}} />,
+                                <Form.HelpText key={p.name+"-h"} text={p.description}></Form.HelpText>]
+                            ))}
+                        {this.state.general && [
+                            <Form.Divider lineColor={ComponentColor.Primary}/>,
+                            <Form.Label required={true} label="Destination Bucket"/>,
+                            <Input value={this.state.destinationBucket} placeholder="Destination Bucket" onChange={this.updateDestinationBucket} />,
+                            <Form.HelpText text="The bucket to write resuls to"/>,
+                            <Form.Divider lineColor={ComponentColor.Primary}/>,
+                            <Form.Label required={true} label="Destination Measurement"/>,
+                            <Input value={this.state.destinationMeasurement} placeholder="Destination Measurement" onChange={this.updateDestinationMeasurement} />,
+                            <Form.HelpText text="The measurement to write resuls to"/>
+                        ]}
+                        {!this.state.general && [
+                            <Form.Divider lineColor={ComponentColor.Primary}/>,
+                            <Form.Label required={false} label="Output Tags"/>,
+                            <Input value={this.state.outputTags} placeholder="Output Tags" onChange={this.updateOutputTags}/>,
+                            <Form.HelpText text="Any additional tags to attach to the results"/>,
+                            <Form.Divider lineColor={ComponentColor.Primary}/>,
+                            <Form.Label required={false} label="Repeat Every"/>,
+                            <Input value={this.state.repeat} placeholder="How often to repeat" onChange={this.updateRepeat}/>,
+                            <Form.HelpText text="If required, how often should this action to be repeated?"/>
+                        ]}
+                    </Tabs.TabContents>
+                </Tabs.Container>
+            </Overlay.Body>
+            <Overlay.Footer>
+                <Button text="Apply" onClick={()=>this.props.onApply(this.state)} />
+                <Button text="Cancel" onClick={this.props.onCancel} />
+            </Overlay.Footer>
+            </Overlay.Container>
+        </Overlay>
         )
     }
 
